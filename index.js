83108967f3916aa4a48f5d9556177f845a7ecdb9
maqsudamadrahimova.9@gmail.com <maqsudamadrahimova.9@gmail.com>
2026-03-11 10:20:13 +0500

    exem

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db-config");
-
+const errorHandler = require("./middleware/error.handler");
+const authRouter = require("./router/auth.routes");
+const categoryRouter = require("./router/category.routes");
+const machineRouter = require("./router/machine.routes");
 const app = express();
+
 app.use(express.json());
 connectDB(); 
-
+app.use("/api/auth", authRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/machine", machineRouter);
+app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- router/auth.routes.js
+++ router/auth.routes.js
@@ -2,7 +2,6 @@ const {Router} = require("express")
 const { register, verify } = require("../controller/auth.controller")
 
 
-
 const authRouter = Router()
 authRouter.post("/register",register)
 authRouter.post("/verify",verify)
